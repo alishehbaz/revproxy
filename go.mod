6e44318e1824995315c899b10d779fa498feaf4d
Ali Shehbaz <alishahbaz072@gmail.com>
2024-12-29 02:37:00 -0500

    feat: add basic origin server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alishehbaz/revproxy
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	port := 8081
+
+	originServerHandler := http.HandlerFunc(
+		func(rw http.ResponseWriter, req *http.Request) {
+			fmt.Printf("[origin server] received request at: %s\n", time.Now())
+
+			response := map[string]string{
+				"time": time.Now().UTC().Format(time.RFC3339),
+			}
+
+			_, _ = fmt.Fprint(rw, "origin server response\n")
+			json.NewEncoder(rw).Encode(response)
+		})
+
+	fmt.Printf("Origin server is up and running on port %d\n", port)
+	log.Fatal(http.ListenAndServe(":8081", originServerHandler))
+
+}
