e9ed19911c72e22d3c04fa8e37385f7a8cee0051
Ali Shehbaz <alishahbaz072@gmail.com>
2025-01-01 18:42:42 -0500

    feat: add basic forwarding

--- reverse/main.go
+++ reverse/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -12,14 +13,26 @@ func main() {
 
 	port := 9081
 
+	originServerURL, err := url.Parse("http://127.0.0.1:8081")
+	if err != nil {
+		log.Fatal("there is something wrong with the origin server url")
+	}
+
 	reverseProxyHandler := http.HandlerFunc(
 		func(rw http.ResponseWriter, req *http.Request) {
 			fmt.Printf("[reverse proxy server] request received at: %s\n", time.Now())
+
+			req.Host = originServerURL.Host
+			req.URL.Host = originServerURL.Host
+			req.URL.Scheme = originServerURL.Scheme
+			req.RequestURI = ""
 
 			response := map[string]string{
 				"source": "Reverse Proxy Server",
 				"time":   time.Now().UTC().Format(time.RFC3339),
 			}
+
+			http.DefaultClient.Do(req)
 
 			fmt.Fprint(rw, "reverse proxy server response\n")
 			json.NewEncoder(rw).Encode(response)
